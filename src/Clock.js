3cdac77c70a2f95596af9daf883b409b358bd51a
unknown <hamad200714@gmail.com>
2023-02-22 19:54:33 +0300

    done

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(interval);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
